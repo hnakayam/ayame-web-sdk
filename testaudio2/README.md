2d0bab8842b4622523822b34133bc0163f65a5e7
Hiroyuki NAKAYAMA <hiron@bunniesfield.com>
2022-04-06 14:55:46 +0900

    creare testaudio2 for mix input/duplicate output support

---
+++ testaudio2/README.md
@@ -0,0 +1,120 @@
+# testaudio
+
+オーディオのみの通話を試すためのテスト用コードです。以下の順番で実行してください。
+
+1. このプロジェクトは node.js を利用してTypeScriptのビルド等を行います。以下を参照して `node.js` および `npm` をインストールしてください。
+
+https://nodejs.org/ja/download/
+
+Windowsの場合は環境変数 `NODE_PATH`に `%APPDATA%\npm\node_modules` を設定してください。
+
+2. npm同様のモジュール管理ツール `yarn` をインストールします。
+
+https://yarnpkg.com/getting-started/install
+
+3. ベースディレクトリで `yarn install` を実行し、依存モジュールをインストールしてください。
+
+2. ベースディレクトリで何らかのスタティックWebサーバーを起動してください。
+
+例えば node-static を利用する場合は、以下のようにインストールした後、
+
+`npm install -g node-static`
+
+ベースディレクトリで以下のように実行します。
+
+`static -c 0 .`
+
+3. WebRTC対応のブラウザで以下のいずれかのURLを開きます。
+
+http://localhost:8080/testaudio/sendonly.html
+
+http://localhost:8080/testaudio/recvonly.html
+
+http://localhost:8080/testaudio/sendrecv.html
+
+
+同じAyameシグナリングサーバーに接続する同じroom IDの2つのブラウザ間でWebRTC通話が可能です。
+
+sendonly → recvonly
+sendrecv ⇔ sendrecv
+
+の2通りのシナリオで動作確認しています。
+
+
+## 動作説明
+
+### sendonly.html
+
+ページを開くとマイクの使用がリクエストされ、その後音声入力が始まります。
+このときオーディオコントロールのミュート解除を行うと、マイク音声のモニタが可能です (ハウリング音が発生する場合はヘッドセット等を利用してください)
+
+その後「接続」ボタンを押すとayameシグナリングサーバーに接続します。すでに接続相手がいるか、あるいは新しく接続相手が接続するとWebRTC通話(ただし送信のみ一方向)が始まります。
+「切断」ボタンを押すか、相手が切断した場合にはWebRTC通話が終了します。再度WebRTC通話を行うには(両側で)再度「接続」ボタンを押してください。
+
+### recvonly.html
+
+このテストではマイクは使用しません。
+
+オーディオコントロールでリモート側音声のボリュームやミュートなどのコントロールを行います。
+
+「接続」ボタンを押すとayameシグナリングサーバーに接続します。すでに接続相手がいるか、あるいは新しく接続相手が接続するとWebRTC通話(ただし受信のみ一方向)が始まります。
+「切断」ボタンを押すか、相手が切断した場合にはWebRTC通話が終了します。再度WebRTC通話を行うには(両側で)再度「接続」ボタンを押してください。
+
+### sendrecv.html
+
+双方向の通話テストを行います。
+
+![sendrecv.html](testaudio_sendrecv.png)
+
+左側のオーディオコントロールでローカル側音声のコントロールを行います。右側のオーディオコントロールでリモート側音声のボリュームやミュートなどのコントロールを行います。
+
+「接続」ボタンを押すとayameシグナリングサーバーに接続します。すでに接続相手がいるか、あるいは新しく接続相手が接続するとWebRTC通話(ただし受信のみ一方向)が始まります。
+「切断」ボタンを押すか、相手が切断した場合にはWebRTC通話が終了します。再度WebRTC通話を行うには(両側で)再度「接続」ボタンを押してください。
+
+左側のオーディオコントロールはミュート解除すると(接続中かどうかに関係なく)ローカル音声のモニタが可能です。モニタ中にWebRTC接続すると、モニタ音声とリモート音声はミックスして出力されます。
+
+## Ayame シグナリングサーバー、Room IDの変更
+
+main.jsの以下の記述を変更してください。
+
+```
+const signalingUrl = 'wss://ayame-labo.shiguredo.jp/signaling';
+let roomId = 'ayame-test-sdk';
+```
+
+## オーディオコーデックの変更
+
+sendonly.html と sendrecv.htmlでは、SDP answer送出時に使用するオーディオコーデックを変更することができます。
+
+Defaultが選択されているときに接続開始すると、SDP answerに使用可能なオーディオコーデックのリストが送出されます。
+
+いずれかのオーディオコーデックを選択すると、これが使用可能な唯一のオーディオコーデックとしてSDP answerが送出されるため、これが選択されます。
+
+また、main.js の以下の部分でMIME TYPEを指定する事で、.htmlファイルロード時に選択されているオーディオコーデックを変更することができます。 nullを指定すると Defaultがそのまま選択されます。
+
+```
+// select 'audio/PCMU' or other MIME type for audio codec initial selection (if any)
+//const codecMimeTypeInitial = 'audio/PCMU'
+const codecMimeTypeInitial = null;
+```
+
+recvonly.html 側で選択する方法は、Ayameシグナリングサーバーとの組み合わせではうまくオーディオコーデックの選択ができませんでした。
+
+## オーディオデバイスの選択
+
+sendonly.html と sendrecv.htmlではオーディオ入力 (マイク)に使用するデバイスを選択可能です。オーディオ入力デバイスの選択はWebRTC接続前のみ可能です。
+
+recvonly.html と sendrecv.htmlではWebRTCで受信した音声を出力するオーディオ出力デバイスを選択可能です。これはいつでも変更可能です。
+
+オーディオ入力をモニタする音声は、(オーディオ出力デバイスの選択に関係なく)デフォルト出力デバイスに出力されます。
+デフォルトの入力/出力オーディオデバイスはWindows10の場合コントロールパネルの「サウンド」や「設定」→「システム」→「サウンド」から変更可能です。
+
+## TypeScript (.ts) ファイルの更新と再ビルド
+
+このディレクトリ内のテスト.htmlファイルは ビルド済みの dist/ayame.js のみを参照します。
+
+ayame-web-sdk の TypeScript (.ts)ファイルを更新し再ビルドするときは、ベースディレクトリで (`yarn install` 実行後に)
+
+`yarn build`
+
+を実行します。
---
+++ testaudio2/main.js
@@ -0,0 +1,202 @@
+// ayame signaling server parameters
+const signalingUrl = 'wss://ayame-labo.shiguredo.jp/signaling';
+let roomId = 'release2.1-test-room1';
+
+// select 'audio/PCMU' or other MIME type for audio codec initial selection (if any)
+const codecMimeTypeInitial = 'audio/PCMU'
+//const codecMimeTypeInitial = null;
+
+let mediaStream;  // for local audio device
+let clientId = null;
+let messages = "";
+let signalingKey = null;
+
+// query string から roomId, clientId を取得するヘルパー
+function parseQueryString() {
+  const qs = window.Qs;
+  if (window.location.search.length > 0) {
+    var params = qs.parse(window.location.search.substr(1));
+    if (params.roomId) {
+      roomId = params.roomId;
+    }
+    if (params.clientId) {
+      clientId = params.clientId;
+    }
+    if (params.signalingKey) {
+      signalingKey = params.signalingKey;
+    }
+  }
+}
+
+parseQueryString();
+
+// setup audio codec selector
+const codecSelector = document.querySelector('select#codec');
+const codecPreferences = document.querySelector('select#codecPreferences');
+const supportsSetCodecPreferences = window.RTCRtpTransceiver &&
+'setCodecPreferences' in window.RTCRtpTransceiver.prototype;
+
+function SetupAudioCodecSelect(sender) {
+  // if either of above selectors not exist, skip following
+  if (!(codecSelector === null || codecPreferences === null)) {
+
+    if (supportsSetCodecPreferences) {
+      codecSelector.style.display = 'none';
+
+      const {codecs} = (sender) ? RTCRtpSender.getCapabilities('audio') : RTCRtpReceiver.getCapabilities('audio');
+
+      codecs.forEach(codec => {
+        if (['audio/CN', 'audio/telephone-event'].includes(codec.mimeType)) {
+          return;
+        }
+        const option = document.createElement('option');
+        option.value = (codec.mimeType + ' ' + codec.clockRate + ' ' +
+          (codec.sdpFmtpLine || '')).trim();
+        option.innerText = option.value;
+        // select specific MimeType for the first selection
+        if (codec.mimeType === codecMimeTypeInitial) {
+          option.defaultSelected = true;
+        }
+        codecPreferences.appendChild(option);
+      });
+      codecPreferences.disabled = false;
+    } else {
+      codecPreferences.style.display = 'none';
+    }
+  }
+}
+
+function GetAudioCodecSelect() {
+  // check "select#codec" then "select#codecPreferences"
+  let codec;
+  if (codecPreferences.disabled) {
+    codec = document.getElementById("codec").value;
+  }
+  else {
+    codec = document.getElementById("codecPreferences").value;
+  }
+  return codec; // return 'none' if Default selected.
+}
+
+// setup audio input/output select
+const audioInputSelect = document.querySelector('select#audioSource');
+const audioOutputSelect = document.querySelector('select#audioOutput');
+const audioSelectors = [audioInputSelect, audioOutputSelect];
+
+audioOutputSelect.disabled = !('sinkId' in HTMLMediaElement.prototype);
+
+// define getUserMedia callback
+function gotDevices(deviceInfos) {
+  // Handles being called several times to update labels. Preserve values.
+  const values = audioSelectors.map(select => select.value);
+  audioSelectors.forEach(select => {
+    while (select.firstChild) {
+      select.removeChild(select.firstChild);
+    }
+  });
+  for (let i = 0; i !== deviceInfos.length; ++i) {
+    const deviceInfo = deviceInfos[i];
+    const option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audioinput') {
+      option.text = deviceInfo.label || `microphone ${audioInputSelect.length + 1}`;
+      audioInputSelect.appendChild(option);
+      console.log('audio input: ', option.text, ` Id: ${deviceInfo.deviceId}`);
+    } else if (deviceInfo.kind === 'audiooutput') {
+      option.text = deviceInfo.label || `speaker ${audioOutputSelect.length + 1}`;
+      audioOutputSelect.appendChild(option);
+      console.log('audio output: ', option.text, ` Id: ${deviceInfo.deviceId}`);
+    } else if (deviceInfo.kind === 'videoinput') {
+      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+      // skip Camera device
+      //videoSelect.appendChild(option);
+      //console.log('Camera source: ', option.text, ` Id: ${deviceInfo.deviceId}`);
+    } else {
+      console.log('Some other kind of source/device: ', deviceInfo);
+    }
+  }
+
+  // set both audioSource and audioOutput
+  audioSelectors.forEach((select, selectorIndex) => {
+    if (Array.prototype.slice.call(select.childNodes).some(n => n.value === values[selectorIndex])) {
+      select.value = values[selectorIndex];
+    }
+  });
+}
+
+// define getUserMedia callback for audio/video streaming start
+function gotStream(stream) {
+  mediaStream = stream;   // remember local stream used
+  //window.stream = stream; // make stream (feedback) available to console
+
+  const audioTracks = mediaStream.getAudioTracks();
+  if (audioTracks.length > 0) {
+    console.log(`Using Audio input device: ${audioTracks[0].label}`);   // echo audio input device
+  }
+
+  // use Audio/Video controls to monitor local video/audio
+  // unmute and use headset to prevent howling audio response
+  document.querySelector('#local-audio').srcObject = mediaStream;
+  //videoElement.srcObject = stream;
+
+  // Refresh button list in case labels have become available
+  return navigator.mediaDevices.enumerateDevices();
+}
+
+function handleError(error) {
+  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+}
+
+// enumerate media devices
+// this also requests user the permission???
+navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+// Attach audio output device to audio/video element using device/sink ID.
+function attachSinkId(element, sinkId) {
+  if (element == null) {
+    // exclude 'null' or 'undefined'
+    console.log(`Invalid element passed: ${element}`);
+  } else if (typeof element.sinkId !== 'undefined') {
+    element.setSinkId(sinkId)
+        .then(() => {
+          console.log(`Success, audio output device changed to: ${sinkId}`);
+        })
+        .catch(error => {
+          let errorMessage = error;
+          if (error.name === 'SecurityError') {
+            errorMessage = `You need to use HTTPS for selecting audio output device: ${error}`;
+          }
+          console.error(errorMessage);
+          // Jump back to first output device in the list as it's the default.
+          audioOutputSelect.selectedIndex = 0;
+        });
+  } else {
+    console.warn('Browser does not support output device selection.');
+  }
+}
+
+// attach selected audio output to remote audio controls
+function changeAudioDestination() {
+  const audioDestination = audioOutputSelect.value;   // get current audioOutput device Id selected
+  const element = document.querySelector('#remote-audio');
+  attachSinkId(element, audioDestination);
+}
+
+// get desired UserMedia for send
+// we only use audioSource, no videoSource used
+function start() {
+  // if (window.stream) {
+  //   window.stream.getTracks().forEach(track => {
+  //     track.stop();
+  //   });
+  // }
+
+  const audioSource = audioInputSelect.value;
+  //const videoSource = videoSelect.value;
+  const constraints = {
+    audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
+    //video: {deviceId: videoSource ? {exact: videoSource} : undefined}
+    video: false
+  };
+  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+}
